ec7d6bf62f24b26660ce2c295babb07818c573c9
ザティル・イマニ <zatilimaniahwan@gmail.com>
2024-09-16 01:02:14 +0800

    remove comment n fix default test

--- src/shared/utils/data-source.ts
+++ src/shared/utils/data-source.ts
@@ -11,6 +11,6 @@ export const dataSource = (
   username: configService.get<string>("DB_USERNAME"),
   password: configService.get<string>("DB_PASSWORD"),
   database: configService.get<string>("DB_NAME"),
-  entities: [Product], // Include your Product entity here
-  synchronize: configService.get<boolean>("DB_SYNCHRONIZE"), // true or false, from the env file
+  entities: [Product],
+  synchronize: configService.get<boolean>("DB_SYNCHRONIZE"),
 });
