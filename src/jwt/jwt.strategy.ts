20e164761ee6e41f91c252cdde9a505b3cc9b0f1
ザティル・イマニ <zatilimaniahwan@gmail.com>
2024-09-16 02:02:40 +0800

    add type checking for payload

--- src/jwt/jwt.strategy.ts
+++ src/jwt/jwt.strategy.ts
@@ -2,6 +2,12 @@ import { Injectable } from "@nestjs/common";
 import { PassportStrategy } from "@nestjs/passport";
 import { ExtractJwt, Strategy } from "passport-jwt";
 
+type JwtPayload = {
+  sub: string;
+  username: string;
+  role: string;
+};
+
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor() {
@@ -11,7 +17,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
       secretOrKey: process.env.JWT_SECRET,
     });
   }
-  async validate(payload: any) {
+  async validate(payload: JwtPayload) {
     return {
       sub: payload.sub,
       username: payload.username,
