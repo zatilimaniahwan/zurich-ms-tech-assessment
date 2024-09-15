a0b9dd92468f25d8422fa84233312c3280caeeac
ザティル・イマニ <zatilimaniahwan@gmail.com>
2024-09-16 01:52:19 +0800

    add comment

--- src/jwt/generate-token.js
+++ src/jwt/generate-token.js
@@ -2,9 +2,11 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var jwt_1 = require("@nestjs/jwt");
 var jwtService = new jwt_1.JwtService({ secret: process.env.JWT_SECRET });
+
+// replace the role and username before generating the token
 var token = jwtService.sign({
-  username: "general",
-  role: "public",
+  username: "admin",
+  role: "admin",
   sub: "12345",
 });
 console.log(token);
